23fe5ecb49b1718bc973391bf6f289672f4c11d8
dionvitor <dionvictor11@gmail.com>
2023-06-17 19:00:58 -0300

    feat: Adicionado chamada ao celery no endpoint de criação de um preenchimento de prosposta de empréstimo.

--- api/infra/celery_gateway/tasks.py
+++ api/infra/celery_gateway/tasks.py
@@ -1,6 +1,10 @@
+from random import randint
+
 from infra.celery_gateway.worker import app
+from loan.models import LoanOffer
 
 
 @app.task()
-def process_personal_loan():
-    pass
+def process_personal_loan(loan_offer_id: str):
+    LoanOffer.objects.filter(id=loan_offer_id).update(status=randint(1, 2))
+    return True
--- api/loan/views.py
+++ api/loan/views.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from loan.models import LoanField, LoanOffer
 from loan.serializers import LoanFieldSerializer, LoanOfferSerializer
 
+from infra.celery_gateway.tasks import process_personal_loan
+
 
 class LoanFieldViewSet(ModelViewSet):
     queryset = LoanField.objects.all()
@@ -12,3 +14,10 @@ class LoanFieldViewSet(ModelViewSet):
 class LoanOfferViewSet(ModelViewSet):
     queryset = LoanOffer.objects.all()
     serializer_class = LoanOfferSerializer
+
+    def create(self, request, *args, **kwargs):
+        create_response = super(LoanOfferViewSet, self).create(request, *args, **kwargs)
+        process_personal_loan.apply_async(
+            kwargs={"loan_offer_id": str(create_response.data.get("id"))}
+        )
+        return create_response
